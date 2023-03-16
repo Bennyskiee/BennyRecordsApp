73687670cd28a46b0cd812bf12dec9d9bce6571d
Bennyskiee <202080019@psu.palawan.edu.ph>
2023-03-16 16:55:17 +0800

    on going work

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+	define('ROOT_URL', 'http://localhost/BennyRecordsApp/apps');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('DB_NAME', 'recordsapp');
+
+	?>
---
+++ include/sidebar.php
@@ -0,0 +1,36 @@
+
+            <div class="sidebar-wrapper">
+                <div class="logo">
+                    <a href="javascript:;" class="simple-text">
+                      Your Logo
+                    </a>
+                </div>
+                <ul class="nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">
+                            <i class="nc-icon nc-icon nc-paper-2"></i>
+                            <p>Dashboard</p>
+                        </a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="office.php">
+                            <i class="nc-icon nc-icon nc-bank"></i>
+                            <p>Offices</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="employee.php">
+                            <i class="nc-icon nc-single-02"></i>
+                            <p>Employee</p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item active active-pro">
+                        <a class="nav-link active" href="javascript:;">
+                            <i class="nc-icon nc-alien-33"></i>
+                            <p>Upgrade plan</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
